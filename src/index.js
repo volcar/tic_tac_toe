7f2f583f3569e694611719d3cb57408a14384c71
Scotty Strahan <svolcar@gmail.com>
2017-05-16 21:01:29 -0400

    Update indes.js

--- src/index.js
+++ src/index.js
@@ -3,19 +3,39 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      value:null,
+    }
+  }
+  
   render() {
     return (
-      <button className="square">
-        {/* TODO */}
+      <button className="square" onClick={() => this.setState({value: 'X'})}>
+        {this.state.value}
       </button>
     );
   }
 }
 
 class Board extends React.Component {
-  renderSquare(i) {
-    return <Square />;
+  constructor() {
+    super();
+    this.state = {
+      squares: Array(9).fill(null),
+    };
   }
+  
+  renderSquare(i) {
+    return (
+      <Square 
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+     />
+    );
+  }
+
 
   render() {
     const status = 'Next player: X';
